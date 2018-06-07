7d2e7a0fbaf74b1c733c0fb57a9c68ed50be4e83
wujianhui321 <wujianhui321@yeah.net>
2018-06-07 16:08:31 +0800

    iii

--- cloud/config/src/main/java/com/wjh/ConfigApplication.java
+++ cloud/config/src/main/java/com/wjh/ConfigApplication.java
@@ -3,7 +3,6 @@ package com.wjh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import sun.security.krb5.Config;
 
 @EnableConfigServer
 @SpringBootApplication
