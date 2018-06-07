1c51cae896b3c82f2e963e77616b288fcfb09847
wujianhui321 <wujianhui321@yeah.net>
2018-06-07 16:57:47 +0800

    ggg

--- cloud/gateway/src/main/java/com.wjh/GatewayApplication.java
+++ cloud/gateway/src/main/java/com.wjh/GatewayApplication.java
@@ -4,13 +4,14 @@ package com.wjh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients
-public class HelloControllerApplication2 {
+@EnableZuulProxy
+public class GatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HelloControllerApplication2.class,args);
+        SpringApplication.run(GatewayApplication.class,args);
+
     }
 }
