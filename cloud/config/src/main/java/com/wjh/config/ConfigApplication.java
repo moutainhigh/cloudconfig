2e2df4188cca6eb5ec955ea4a77d83edb6040a90
wujianhui321 <wujianhui321@yeah.net>
2018-06-12 21:38:06 +0800

    dfd

--- cloud/config/src/main/java/com/wjh/config/ConfigApplication.java
+++ cloud/config/src/main/java/com/wjh/config/ConfigApplication.java
@@ -2,16 +2,16 @@ package com.wjh.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan("com.wjh")
 public class ConfigApplication {
     public static void main(String[] args) {
-
-
 
         SpringApplication.run(ConfigApplication.class,args);
 
